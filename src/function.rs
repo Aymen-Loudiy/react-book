540e8e90709bc195f6a0b914da587c7fea98f309
Aymen Loudiy <91712474+Aymen-Loudiy@users.noreply.github.com>
2024-12-18 20:52:58 -0500

    finish : functions chapter

--- src/function.rs
+++ src/function.rs
@@ -7,3 +7,10 @@ fn main() {
 fn another_function() {
     println!("Another function.");
 }
+fn statement_vs_expression() {
+    let statement = 5;
+    let expression = {
+        let x = 5;
+        x + 1
+    };
+}
